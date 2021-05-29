dc3d656bc81c9f90eb39d330cc0e42ad10ef7fc8
Trent Julich <tjulich1@gmail.com>
2021-05-28 19:04:13 -0700

    switching machines

--- src/4.1/main.cpp
+++ src/4.1/main.cpp
@@ -1,9 +1,60 @@
-// Determine if path from A -> B exists in graph.
+// Trent Julich ~ 28 May 2021
 
-int main(int argc, char* argv[]) {
+#include <iostream>
+#include <vector>
+#include <queue>
+
+struct GraphNode 
+{
+  int value;
+  std::vector<GraphNode*> neighbors;
+};
+
+// Determine if path from A -> B exists in graph.
+bool Solution(GraphNode* pStart, GraphNode* pEnd) 
+{
+  bool found = false;
+
+  std::queue<GraphNode*> visitQueue;
+
+  visitQueue.push(pStart);
+
+  while (!visitQueue.empty() && !found) 
+  {
+    GraphNode* currentNode = visitQueue.front();
+    visitQueue.pop();
+
+    for (int i = 0; i < currentNode->neighbors.size(); i++) {
+      std::cout << "i: " << i << std::endl;
+      if (currentNode->neighbors[i] == pEnd) {
+        found = true;
+        break;
+      }
+      visitQueue.push(currentNode->neighbors[i]);
+    }
+  }
+  return found;
+}
+
+int main(int argc, char* argv[]) 
+{
   int status = 0;
 
-  std::cout<<"Hello, world!"<<std::endl;
+  GraphNode first, second, third, fourth;
+
+  first.value = 1;
+  second.value = 2;
+  third.value = 3;
+
+  std::vector<GraphNode*> firstNeighbors = {&second, &third};
+  std::vector<GraphNode*> secondNeighbors = {&first};
+  std::vector<GraphNode*> thirdNeighbors = {&first, &second};
+
+  first.neighbors = firstNeighbors;
+  second.neighbors = secondNeighbors;
+  third.neighbors = thirdNeighbors;
+
+  std::cout<<"Path exists? "<<Solution(&second, &fourth)<<std::endl;
 
   return status;
 }
